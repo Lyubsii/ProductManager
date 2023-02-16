cc6af318fd886f014d2eaf3f47507bcda4742dcb
Lyubsii <gordyuhina_lyuba@mail.ru>
2023-02-16 21:40:02 +0300

    add tests commit

---
+++ src/test/java/ru/netology/ManagerTest.java
@@ -0,0 +1,100 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+
+
+    @Test
+    public void searchTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер 1", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+
+        Product[] expected = {book};
+        Product[] actual = manager.searchBy("Гарри");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchTest2() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+        String name = "Бутылка";
+
+        Product[] expected = {product};
+        Product[] actual = manager.searchBy(name);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        manager.add(smartphone);
+
+        Product[] expected = {smartphone};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchWhenMoreThanOneItemTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Гарри Поттер", 100, "Джоан Роулинг");
+        Book book2 = new Book(2, "Гарри Поттер", 100, "Джоан Роулинг");
+        Book book3 = new Book(3, "Лавр", 1000, "Водолазкин");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        String name = "Гарри";
+
+        Product[] expected = {book1, book2};
+        Product[] actual = manager.searchBy(name);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addAllTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        manager.add(smartphone);
+        manager.add(book);
+        manager.add(product);
+
+        Product[] expected = {smartphone, book, product};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+}
---
+++ src/test/java/ru/netology/RepositoryTest.java
@@ -0,0 +1,97 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RepositoryTest {
+
+    @Test
+    public void addTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер 1", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+
+        Product[] expected = {book, smartphone, product};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddOneProduct() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер 1", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        repo.add(smartphone);
+        Product[] expected = {smartphone};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldAddAllTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер 1", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        repo.add(smartphone);
+        repo.add(book);
+        repo.add(product);
+        Product[] expected = {smartphone, book, product};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер 1", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        repo.add(book);
+        repo.add(smartphone);
+        repo.add(product);
+        repo.removeById(2);
+        Product[] expected = {book, product};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveByIdAllTest() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book = new Book(1, "Гарри Поттер 1", 100, "Джоан Роулинг");
+        Smartphone smartphone = new Smartphone(2, "Редми", 20000, "Производитель");
+        Product product = new Product(3, "Бутылка", 50);
+
+        repo.add(book);
+        repo.add(smartphone);
+        repo.add(product);
+        repo.removeById(1);
+        repo.removeById(2);
+        repo.removeById(3);
+
+        Product[] expected = {};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
